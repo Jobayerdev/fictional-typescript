0789c08aed9448034439a7bd2f6f7f269ff36873
Jobayer Hossain <jobayerhossain977@gmail.com>
2022-05-12 17:49:01 +0600

    master: Added

---
+++ 02_factory/example/src/logger.ts
@@ -0,0 +1,26 @@
+// logger class definition singleton pattern implementation with log4js module for logging purposes.
+
+export class Logger {
+	private static instance: Logger
+	private static config: LoggerConfig = {
+		showLog: true,
+	}
+	private constructor() {}
+	public static getInstance(): Logger {
+		if (!Logger.instance) {
+			Logger.instance = new Logger()
+		}
+		return Logger.instance
+	}
+	public log(message: string, ...optionalParams: any[]) {
+		if (Logger.config.showLog) {
+			console.log(message, ...optionalParams)
+		}
+	}
+	public setConfig(logger: LoggerConfig) {
+		Logger.config = logger
+	}
+}
+interface LoggerConfig {
+	showLog: boolean
+}
